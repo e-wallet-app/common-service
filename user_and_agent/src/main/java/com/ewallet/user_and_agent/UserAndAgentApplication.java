22e75a82d6bc2ad682ef154513226accd7197a65
Alamin <alamin8740@gmail.com>
2022-08-01 20:22:50 +0600

    Created user_and_agent module

---
+++ user_and_agent/src/main/java/com/ewallet/user_and_agent/UserAndAgentApplication.java
@@ -0,0 +1,13 @@
+package com.ewallet.user_and_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAndAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserAndAgentApplication.class, args);
+    }
+
+}
