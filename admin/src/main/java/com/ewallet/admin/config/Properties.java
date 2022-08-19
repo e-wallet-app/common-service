244a7134636acddfe1007971d676078751b42dd2
Animesh245 <bdanimesh245@gmail.com>
2022-08-19 07:09:23 +0600

    Changed nid_photo folder path

--- admin/src/main/java/com/ewallet/admin/config/Properties.java
+++ admin/src/main/java/com/ewallet/admin/config/Properties.java
@@ -5,5 +5,5 @@ public class Properties
     public static final String WRITE_PATH = System.getProperty("user.home") + "/Documents/ewallet/";
 
     public static final String Attachment_FOLDER= "attachment/";
-    public static final String NID_CARD_FOLDER= "status/";
+    public static final String NID_CARD_FOLDER= "nid_card/";
 }
