04e9dc464b50c4f0149779ebb86eb53a796c8db8
Animesh245 <bdanimesh245@gmail.com>
2022-08-19 07:07:55 +0600

    Admin Id datatype changed to UUID

--- admin/src/main/java/com/ewallet/admin/repository/AdminRepository.java
+++ admin/src/main/java/com/ewallet/admin/repository/AdminRepository.java
@@ -4,6 +4,8 @@ import com.ewallet.admin.entity.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface AdminRepository extends JpaRepository<Admin, Long> {
+public interface AdminRepository extends JpaRepository<Admin, UUID> {
 }
