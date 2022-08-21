9f7879d18bcaef05e48c765700278c7bbc78077e
Animesh245 <bdanimesh245@gmail.com>
2022-08-21 15:16:54 +0600

    Exceptions

---
+++ admin/src/main/java/com/ewallet/admin/exceptions/ItemValidationError.java
@@ -0,0 +1,18 @@
+package com.ewallet.admin.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItemValidationError
+{
+    private String itemName;
+    private String field;
+    private Object rejectedValue;
+    private String message;
+}
--- admin/src/main/java/com/ewallet/admin/exceptions/NotFoundException.java
+++ admin/src/main/java/com/ewallet/admin/exceptions/NotFoundException.java
@@ -1,4 +1,4 @@
-package com.ewallet.admin.exception;
+package com.ewallet.admin.exceptions;
 
 public class NotFoundException extends RuntimeException
 {
